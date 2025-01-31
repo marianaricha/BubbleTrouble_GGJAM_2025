52dbd313051c0b7ab14789b4babca26f6fd826ce
luanalauschner <vilarinholuana@gmail.com>
2025-01-31 17:43:36 -0300

    Correção da camera nas outras fases

--- Assets/FriendPrefabController.cs
+++ Assets/FriendPrefabController.cs
@@ -6,10 +6,11 @@ public class FriendPrefabController : MonoBehaviour
 {   
     //public GameObject SocialBubble;
     public GameObject friendGameObject;
+    private int ContCollider = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Numero de colisões: " + ContCollider);
     }
 
     // Update is called once per frame
@@ -20,5 +21,7 @@ public class FriendPrefabController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         friendGameObject.SetActive(false);
+        ContCollider+=1;
+        Debug.Log("Colisões: " + ContCollider);
     }
 }
--- Assets/FriendsController.cs
+++ Assets/FriendsController.cs
@@ -11,9 +11,10 @@ public class FriendsController : MonoBehaviour
     public GameObject Friends;
     private SocialBubbleController socialBubbleController;
 
-    private float areaX = -6f;
-    private float areaY = -19f;
-    private float areaYY = 16f;
+    private float areaXPositiva = 15f;
+    private float areaXNegativa = -8f;
+    private float areaYPositiva = 6f;
+    private float areaYNegativa = -10f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,13 +28,13 @@ public class FriendsController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        objectPoolFriends.GetFromPool();
+        //objectPoolFriends.GetFromPool();
     }
     private void randomFriend(){
         GameObject friend = objectPoolFriends.GetFromPool();
         if (friend != null){
-            float randomX = Random.Range(-areaX, areaX);
-            float randomY = Random.Range(areaY, areaYY);
+            float randomX = Random.Range(areaXNegativa, areaXPositiva);
+            float randomY = Random.Range(areaYNegativa, areaYPositiva);
             Vector3 friendPosition = new Vector3(randomX, randomY, 0);
 
             friend.transform.position = friendPosition;
